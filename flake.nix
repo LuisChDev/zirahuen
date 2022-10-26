cf9ef002797835baead600c2d8e269e29b2216d9
Luis Chavarriaga <luischa123@gmail.com>
2022-10-26 01:32:25 +0000

    add flake files

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Zirahuen's expo environment (with Android)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          android_sdk.accept_license = true;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ androidenv.androidPkgs_9_0.androidsdk ];
+        };
+      });
+}
